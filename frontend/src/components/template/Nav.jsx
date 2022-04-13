d475b3e0cc51f3b2bb8189cf552fb21377a95444
mirandaeverton <mir.everton@gmail.com>
2022-04-13 21:08:47 +0100

    Routes implemented

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,10 +1,11 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            <a href="#/" className="fa fa-home"> Início</a>
-            <a href="#/users" className="fa fa-users"> Users</a>
+            <Link to="/" className="fa fa-home"> Início</Link>
+            <Link to="/users" className="fa fa-users"> Users</Link>
         </nav>
     </aside>
